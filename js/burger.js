0feb5cc4c3f3ba4efc41b6fe37347af6415cabe0
truemilky <truemilky@gmail.com>
2020-11-22 10:08:52 +0300

    Modified Mobile Version

--- js/burger.js
+++ js/burger.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
     $('.burger__span').toggleClass('burger__span-active');
     $('body').toggleClass('active');
   })
+
+  $('.header__list').click(function(event){
+    $('.header__inner,.header__burger-opened').toggleClass('header__active');
+    $('.header__burger').toggleClass('header__burger-active');
+    $('.burger__span').toggleClass('burger__span-active');
+    $('body').toggleClass('active');
+  })
+
+  $('.header__contacts').click(function(event){
+    $('.header__inner,.header__burger-opened').toggleClass('header__active');
+    $('.header__burger').toggleClass('header__burger-active');
+    $('.burger__span').toggleClass('burger__span-active');
+    $('body').toggleClass('active');
+  })
 })
